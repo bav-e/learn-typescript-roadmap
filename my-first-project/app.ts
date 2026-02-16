893161a4d52a0ec6fb6d9ad851a4456d648ad1b8
Bawantha Dulara <118605430+bav-e@users.noreply.github.com>
2026-02-16 14:52:53 +0530

    Learned how to loop arrays

--- my-first-project/app.ts
+++ my-first-project/app.ts
@@ -1,13 +1,22 @@
 const shopName: string = "Myshop";
-let itemPrice: number = 150;
+let itemPrice = [100, 150, 500, 1000];
 let isPaid: boolean = true;
 let totalBill = 1500; //it is not nessesary to add type in variables
-let itemCount:number = 10
+let itemCount: number = 10
 const taxRate: number = 0.15;
-// let taxAmount: number = itemPrice * taxRate;
-let finalPrice: number = itemPrice + itemPrice * taxRate;
+
+let finalPrice: number = itemPrice[1] + itemPrice[1] * taxRate;
 
 console.log("Welcome to ", shopName);
+
+for (let i = 0; i < itemPrice.length; i++) {//old method
+    console.log(i + 1, itemPrice[i]);
+}
+
+itemPrice.forEach((price, i)=>{
+    console.log(i+1,price);
+});
+
 
 if (totalBill > 1500) {
     console.log("Price is Rs.", finalPrice * itemCount - 150);
