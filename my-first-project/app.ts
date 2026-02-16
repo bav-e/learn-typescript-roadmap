7613755ae38928b911c3e24c1e1123c1ea931f1d
Bawantha Dulara <118605430+bav-e@users.noreply.github.com>
2026-02-16 14:37:15 +0530

    Learnt how to use if/else

--- my-first-project/app.ts
+++ my-first-project/app.ts
@@ -1,10 +1,16 @@
 const shopName: string = "Myshop";
-let itemPrice: number = 150.0;
+let itemPrice: number = 150;
 let isPaid: boolean = true;
-
+let totalBill = 1500; //it is not nessesary to add type in variables
+let itemCount:number = 10
 const taxRate: number = 0.15;
 // let taxAmount: number = itemPrice * taxRate;
-let finalPrice: number = itemPrice + (itemPrice * taxRate);
+let finalPrice: number = itemPrice + itemPrice * taxRate;
 
 console.log("Welcome to ", shopName);
-console.log("price is Rs.", finalPrice);
+
+if (totalBill > 1500) {
+    console.log("Price is Rs.", finalPrice * itemCount - 150);
+} else {
+    console.log("price is Rs.", finalPrice * itemCount, "[No Discount]");
+}
