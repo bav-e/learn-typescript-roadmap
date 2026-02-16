85517f76cefbb016cfbd6e688ec7a18407c307e9
Bawantha Dulara <118605430+bav-e@users.noreply.github.com>
2026-02-16 14:26:45 +0530

    Learnet how to declare variables and do arithmetic operations

--- my-first-project/app.ts
+++ my-first-project/app.ts
@@ -1,1 +1,10 @@
-console.log("Welcome to the Shop")
+const shopName: string = "Myshop";
+let itemPrice: number = 150.0;
+let isPaid: boolean = true;
+
+const taxRate: number = 0.15;
+// let taxAmount: number = itemPrice * taxRate;
+let finalPrice: number = itemPrice + (itemPrice * taxRate);
+
+console.log("Welcome to ", shopName);
+console.log("price is Rs.", finalPrice);
